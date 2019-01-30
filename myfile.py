f6f860558b3f479053d6b1183d1ae8459a3606bd
Jude666 <zh415944@dal.ca>
2019-01-30 12:00:01 -0800

    secondTry

--- myfile.py
+++ myfile.py
@@ -3,11 +3,20 @@
 import os
 
 def walk(dir):
+    path_list = []
     for name in os.list(dir):
-        path = os.path.join(dir, name)
-        if os.path.isfile(path):
-            print(path)
-        else:
-            walk(path)
+       path = os.path.join(dir, name)
+       if os.path.isfile(path):
+           path_list.append(path)
+       else:
+           walk(path)
+    return path_list
 
-print(walk(dir))
+def reverse(walk(dir)):
+    path_list = path_list[::-1]
+    print(path_list)
+
+
+reverse(walk(dir))
+'''This is the code modifies'''
+'''And this is the program to print all the folders in reverse'''
